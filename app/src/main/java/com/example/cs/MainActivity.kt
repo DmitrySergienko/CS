66d3556407f23a9779d3c1a816b779355c9abd7c
DmitrySergienko <dmitrisergienko@gmail.com>
2022-05-31 10:44:31 +0400

    initial commit

--- app/src/main/java/com/example/cs/MainActivity.kt
+++ app/src/main/java/com/example/cs/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cs.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +12,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+
 }
+
